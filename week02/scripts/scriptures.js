9f78a5d3dff0abb99bb5ecea591f019daebc7e82
sharedlopezserrano <slopezserrano@byupathway.edu>
2025-05-16 14:55:36 -0500

    Week 02 DOM events

--- week02/scripts/scriptures.js
+++ week02/scripts/scriptures.js
@@ -1,9 +1,23 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("_______");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector("ul");
+
+button.addEventListener("click", function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = "";
+        input.focus();
+    }
+});
+
